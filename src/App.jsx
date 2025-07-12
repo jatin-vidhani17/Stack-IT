070a9b6bdd238a5ac488361cdb7e4539c3f5a902
Harshil Dodwani <dodwaniharshil@gmail.com>
2025-07-12 13:38:42 +0530

    feat: pushed changes to master branch

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/create-question" element={<CreateQuestion />} />
     </Routes>
   )
